69b355007c15d58e3c94dea3b49dc43085946944
July <majuliabonite@gmail.com>
2024-12-13 20:59:11 +0800

    Debug images

--- README copy.md
+++
@@ -1,3 +0,0 @@
-# odin-restaurant-page
-
-This project was made in partial fulfillment of The Odin Project, JavaScript path.
--- webpack.config.js
+++ webpack.config.js
@@ -7,55 +7,35 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true, // Clean output directory before every build
+    clean: true,
   },
-  devtool: "eval-source-map", // For better debugging
+  devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"], // Watching template for changes
+    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html", // Template for HTML file
+      template: "./src/template.html",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"], // Process CSS files
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.html$/i,
-        loader: "html-loader", // Process HTML files
+        loader: "html-loader",
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i, // Match image files
-        use: [
-          {
-            loader: "file-loader", // Use file-loader
-            options: {
-              name: "[name].[hash].[ext]", // Name pattern for output images
-              outputPath: "assets/", // Place images in the 'assets' folder
-            },
-          },
-        ],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
       {
-        test: /\.js$/, // Match .js and .jsx files
-        exclude: /node_modules/, // Exclude node_modules for speed
-        use: {
-          loader: "babel-loader", // Use Babel for JavaScript files
-        },
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"], // Resolve .js and .jsx files
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"), // Where Webpack puts your assets
-    },
-    watchFiles: ["./src/template.html"],
   },
 };
